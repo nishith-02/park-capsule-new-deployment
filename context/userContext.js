33aeddb24d872bec0bf1ad352e7bafdcfe704993
Sai Nishith <72185635+nishith-02@users.noreply.github.com>
2024-11-18 23:01:53 +0530

    Update userContext.js

--- context/userContext.js
+++ context/userContext.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
 
 // Create the context
-const UserContext = createContext();
+const UserContext = createContext()
 
 // Provide the context
 export const UserProvider = ({ children }) => {
